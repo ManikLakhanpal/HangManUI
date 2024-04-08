8db61ad332b3344af2a2a82d4c13e317cccc1e3a
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-04-08 15:41:59 +0530

    Now multiple users can play the game.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask, redirect, render_template, url_for, request, jsonify
+from flask import Flask, redirect, render_template, request, session
 import psycopg2
 import random
 
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'  # Set a secret key for session encryption
+
+# Database connection
 connect = psycopg2.connect(
     database="zvkklrad",
     user="zvkklrad",
@@ -11,14 +15,6 @@ connect = psycopg2.connect(
 )
 
 cursor = connect.cursor()
-
-lives =6
-word = []
-answer = []
-hint = ""
-
-app = Flask(__name__)
-app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
@@ -39,43 +35,41 @@ def main():
 
     print(data, len(data))
 
-    global word, answer, lives, hint, question
     number = random.randint(0, len(data) - 1)
     question = data[number][1]
-    word = list(question)
-    answer = []
-    lives = 6
-    hint = data[number][2]
+    session['word'] = list(question)
+    session['answer'] = ['_' for _ in question]
+    session['lives'] = 6
+    session['hint'] = data[number][2]
 
-
-    return render_template("game.html", title="Main", hint=hint, lives=lives, len=len(question))
+    return render_template("game.html", title="Main", hint=session['hint'], lives=session['lives'], answer=session['answer'])
 
 @app.route('/game', methods=['POST'])
 def game():
-    print(request.form)
-    userChar = request.form.get("answer")
+    user_char = request.form.get("answer")
 
-    global lives, answer
-    if len(answer) != len(word):
-        for i in range(len(word)):
-            answer.append("_")
+    if len(session['answer']) != len(session['word']):
+        session['answer'] = ['_' for _ in session['word']]
 
-    if userChar not in word:
-        lives -= 1
-    
-    elif userChar in word:
-        for i in range(len(word)):
-            if word[i] == userChar:
-                answer[i] = userChar
-    
-    if "_" not in answer:
+    if user_char not in session['word']:
+        session['lives'] -= 1
+
+    else:
+        for i in range(len(session['word'])):
+            if session['word'][i] == user_char:
+                session['answer'][i] = user_char
+            else:
+                print(f"No match at index {i}")
+        session['answer'] = session['answer']
+
+
+    if "_" not in session['answer']:
         return "You Win <a href='/main'>Start new game</a>"
 
-    print(userChar, word, answer)
-    if lives <= 0:
+    if session['lives'] <= 0:
         return "You Lose"
-    else:
-        return render_template("gameRun.html", title="Game", hint=hint, lives=lives, answer=answer)
+
+    return render_template("gameRun.html", title="Game", hint=session['hint'], lives=session['lives'], answer=session['answer'])
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
